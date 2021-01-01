956531714bc1d1d5ec34e70f44be8a3af7bedee3
LucaGhilardi <76250241+LucaGhilardi@users.noreply.github.com>
2021-01-01 16:30:19 +0100

    Update smtscrll.js

--- smtscrll.js
+++ smtscrll.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {    
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
+   $("a").on('click', function(event) {    
+      if (this.hash !== "") {        
         event.preventDefault();      
-        var hash = this.hash;      
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        var hash = this.hash;       
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 800, function(){        
